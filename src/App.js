34d1950c1a6ae9c8c0c23fbec9ba9ab6b6dfe7d3
yaminmhd <mdyamin.drzee@gmail.com>
2018-06-17 17:28:55 +0800

    Implemented okta login and signinwidget.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+import Login from './components/auth/Login';
+
+const onAuthRequired = ({ history }) => {
+  history.push("/login");
+};
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/staff" exact={true} component={Staff} />
+        <Security
+          issuer="https://dev-361694.oktapreview.com/oauth2/default"
+          client_id="0oafhm4eyc39A3ArJ0h7"
+          redirect_uri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-361694.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
           </div>
-        </div>
+        </Security>
       </Router>
     );
   }
